34b222f49ba958a959a55af80f13190adffe93a7
System Administrator <ttcool@live.cn>
2016-09-01 12:12:03 +0800

    histogram

---
+++ histogram.py
@@ -0,0 +1,12 @@
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+h = histogram('brontosaurus')
+
+print h
