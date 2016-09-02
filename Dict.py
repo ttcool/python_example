52de020c2f500ecf304b0f4f06c4ced78e53e108
System Administrator <ttcool@live.cn>
2016-09-02 14:40:39 +0800

    Dict.py

--- Dict.py
+++ Dict.py
@@ -1,4 +1,21 @@
 from collections import OrderedDict
+
+from collections import deque
+
 quotes = OrderedDict([('Moe', 'A wise guy, huh?'),('Larry', 'Ow!'),('Curly', 'Nyuk nyuk!'),])
 for stooge in quotes:
     print(stooge)
+
+
+def palindrome(word):
+    dq = deque(word)
+    while len(dq) > 1:
+        if dq.popleft() != dq.pop():
+            return False
+        return True
+
+print palindrome('a')
+
+print palindrome('racecar')
+
+print palindrome('halibut')
